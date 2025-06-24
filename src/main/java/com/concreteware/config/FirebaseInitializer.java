e5fbf89f4c9245f8288ac09442fb77bd1c37db61
carlosrueda13 <82956878+carlosrueda13@users.noreply.github.com>
2025-06-24 03:15:16 -0500

    Update FirebaseInitializer.java

--- src/main/java/com/concreteware/config/FirebaseInitializer.java
+++ src/main/java/com/concreteware/config/FirebaseInitializer.java
@@ -17,7 +17,7 @@ public class FirebaseInitializer {
     @PostConstruct
     public void initialize() {
         try {
-            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("ServiceAccountKey.json");
+            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("GOOGLE_CREDENTIALS_JSON");
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
